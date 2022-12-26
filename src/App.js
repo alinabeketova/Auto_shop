9c59f0ee68edf7c23b391f17d87cc8a0ee115931
Alina <beketova2007@bk.ru>
2022-12-26 20:24:21 +0300

    img

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/header/header';
 import Page from './pages/page';
+import img1 from './img/1.jpg'
+import img2 from './img/2.jpg'
+import img3 from './img/3.jpg'
+
 
 const img67 ='https://www.ljplus.ru/img4/m/i/miss_apriori/40743208.jpg'
 
@@ -10,9 +14,9 @@ function App() {
     <div className="wrapper">
       <Header />
       <Routes>
-        <Route path='/' element={<Page img='img/1.jpg' title='модель 01'/>}/>
-        <Route path='/shop' element={<Page img='img/2.jpg' title='модель 02'/>}/>
-        <Route path='/products' element={<Page img='img/3.jpg' title='модель 03'/>}/>
+        <Route path='/' element={<Page img={img1} title='модель 01'/>}/>
+        <Route path='/shop' element={<Page img={img2} title='модель 02'/>}/>
+        <Route path='/products' element={<Page img={img3} title='модель 03'/>}/>
         <Route path='*' element={<Page img={img67} title='модель 04'/>}/>
       </Routes>
     </div>
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import Nav from '../nav/nav'
 import './header.sass'
+import logo from '../../img/logo.svg'
+import menu from '../../img/menu.svg'
 
 const Header = () => {
     return(
         <div className="conteiner">
             <header className="header">
-                <img src="img/logo.svg" className="logo" alt='ok'/>
+                <img src={logo} className="logo" alt='ok'/>
                 <Nav />
-                <img src="img/menu.svg" className="menu-" alt='ok'/>
+                <img src={menu} className="menu-" alt='ok'/>
             </header>
         </div>
     )
